801b360ce38cf9a0713c84f945313cb3b584dcdc
Amaya Bryant <100623332+amayab9@users.noreply.github.com>
2023-02-13 16:01:50 -0500

    fixed the layout issue on index page

--- assignment_02/PetQuery.js
+++ assignment_02/PetQuery.js
@@ -12,18 +12,20 @@ const setCurrentPage = (pageNum, content) => {
     content.pet.forEach((item, index) => {
         if (index >= prevRange && index < currRange) {
             document.getElementById('pageListing').innerHTML += `
-            <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                  <div class="card-body">
-                    <p class="card-text">${item.petName}<br />
-                                        ${item.breed} &emsp;  &emsp;${item.gender}<br />
-                                        ${item.age}<br />
-                                        ${item.location}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button href="detail.html?index=${item.ID}" type="button" class="btn btn-sm btn-outline-secondary">View</button>
+            <div class="col">
+              <div class="card shadow-sm">
+                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body">
+                      <p class="card-text">${item.petName}<br />
+                                          ${item.breed} &emsp;  &emsp;${item.gender}<br />
+                                          ${item.age}<br />
+                                          ${item.location}</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button href="detail.html?index=${item.ID}" type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        </div>
+                        <small class="text-muted">${item.petType}</small>
                       </div>
-                      <small class="text-muted">${item.petType}</small>
                     </div>
                   </div>
                 </div>`
